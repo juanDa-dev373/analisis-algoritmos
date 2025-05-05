0bcb2deea8819c99ebc3e2e4bae4737285e01831
danielasofia05 <danielas.alzatef@uqvirtual.edu.co>
2025-05-04 22:03:28 -0500

    Se agrega el front

--- analisis/urls.py
+++ analisis/urls.py
@@ -3,9 +3,18 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-
 urlpatterns = [
-    path('', views.menu_ordenamiento, name='menu_ordenamiento'),
-    path('loading/<str:method>/', views.loading_screen, name='loading_screen'),
-    path('sort/<str:method>/', views.sort, name='sort_books'),
+  
+    path('', views.menu_general, name='menu_general'),
+    
+  
+    path('ordenamiento/', views.menu_ordenamiento, name='menu_ordenamiento'),
+    path('ordenamiento/loading/<str:method>/', views.loading_screen, name='loading_screen'),
+    path('ordenamiento/sort/<str:method>/', views.sort, name='sort_books'),
+  
+    path('analisis/', views.menu_analysis_methods, name='menu_analysis_methods'),
+    path('analisis/frecuencia/', views.analizar_frecuencia, name='analizar_frecuencia'),
+    path('analisis/dendograma/', views.generar_dendograma, name='generar_dendograma'),
+    path('analisis/nube-palabras/', views.generar_nube_palabras, name='generar_nube_palabras'),
+    path('analisis/graficas-frecuencia/', views.graficas_frecuencia, name='graficas_frecuencia'),
 ]
