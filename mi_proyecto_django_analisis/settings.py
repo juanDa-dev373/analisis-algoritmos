8945111fc447a8afebfc461bcad0a26aae890481
juanManuelMontoya <juanpirry1396@hotmail.com>
2025-05-17 09:06:07 -0500

    Fix STATIC_ROOT for collectstatic on Render

--- mi_proyecto_django_analisis/settings.py
+++ mi_proyecto_django_analisis/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Opcional para producción:
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
